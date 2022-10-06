c55b761a87b07956e45534e7e4f262ea7390b4de
jcnpereira <casimiro@ipt.pt>
2022-10-06 17:15:27 +0100

    add a button to present nickname to screen

--- app/src/main/java/pt/ipt/dam2022/acercademim/MainActivity.kt
+++ app/src/main/java/pt/ipt/dam2022/acercademim/MainActivity.kt
@@ -10,11 +10,34 @@ import android.widget.TextView
 import android.widget.EditText
 
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // find the button
+        findViewById<Button>(R.id.btShowNickname).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        // write here the button action
+        // make a reference to the textView (to present the nickname)
+        val nicknameText = findViewById<TextView>(R.id.nicknameTextView)
+        // make a reference to the textbox where user will write the nickname
+        val nicknameTextBox = findViewById<EditText>(R.id.nicknameTextBox)
+
+        // write the nickname to the TextView
+        nicknameText.text = nicknameTextBox.text
+        // change visibility
+        nicknameText.visibility = View.VISIBLE
+        nicknameTextBox.visibility = View.GONE
+        view.visibility = View.GONE // hide the button
+
+        // hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
 
     }
 }
