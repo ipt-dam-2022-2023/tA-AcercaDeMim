7e0278510df633db61c4cf274042da09b0df5c8f
jcnpereira <casimiro@ipt.pt>
2022-10-06 15:45:05 +0100

    add of Style to format a TextView

--- app/src/main/java/pt/ipt/dam2022/acercademim/MainActivity.kt
+++ app/src/main/java/pt/ipt/dam2022/acercademim/MainActivity.kt
@@ -1,11 +1,20 @@
 package pt.ipt.dam2022.acercademim
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.TextView
+import android.widget.EditText
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
